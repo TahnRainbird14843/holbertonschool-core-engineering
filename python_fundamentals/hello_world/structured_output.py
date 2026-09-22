5fb859b839121672c0a101fc3d9ffb0e09c63d47
Tahn Rainbird <14843@holbertonstudents.com>
2026-09-22 02:17:01 +0200

    fix

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 def main():
-    print("Language: %s" % ("python"))
-    print("Version: {}".format(3))
-    print("Pi approx:", round(3.14159265, 2), "\nComputation valid:", 2 == 2)
+    print("Language: %s" % ("Python"))
+    print("Version: {}".format("3"))
+    print("Pi approx:", round(3.14159265, 2))
+    print("Computation valid:", 2 == 2)
 
 
 main()
