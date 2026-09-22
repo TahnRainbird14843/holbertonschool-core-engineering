d3a4df9d325d0857b4ca14391bc37c7fd985acaa
Tahn Rainbird <14843@holbertonstudents.com>
2026-09-22 02:01:38 +0200

    formatted string code

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Language: %s" % ("python"))
+    print("Version: {}".format(3))
+    print("Pi approx:", round(3.14159265, 2), "\nComputation valid:", 2 == 2)
+
+
+main()
